a1deb242b60ad5d4646809b7761532c58517fe9f
grace-official <76984418+grace-official@users.noreply.github.com>
2023-01-16 11:52:33 -0800

    e

--- FileIO/Runner.py
+++ FileIO/Runner.py
@@ -3,5 +3,28 @@ from FileWrite import FileWrite
 from FileEncrypter import FileEncrypter
 from FileDecrypter import FileDecrypter
 
+reader = FileRead()
+writer = FileWrite()
+encrypt = FileEncrypter()
+
+# 1. ASCII values and casting
+# Cesarean Cipher
+
+"""
+letter = 'a'
+num = ord(letter)
+print(num)
+newLetter = chr(num + 1)
+print(newLetter)
+"""
+
+# 2. How to change the characters in a string
+
+word = 'Hello'
+
+wordData = list(word)
+print(wordData)
+
+# 3. Basic encryption
 
 
